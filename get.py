244c93b904823a32843201e2424cd2c9a8226e15
cmk666 <72393737+cmk666@users.noreply.github.com>
2022-10-12 12:47:54 +0800

    Add files via upload

---
+++ get.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup as bs
+import requests as req
+from os.path import join as pjoin
+def get(idx):
+	print(f'Getting {idx}...')
+	try:
+		url = f'https://www.luogu.com.cn/problem/P{idx}'
+		res = req.get(url, headers = {
+			'User-Agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; Tablet PC 2.0; wbx 1.0.0; wbxapp 1.0.0; Zoom 3.6.0)'
+		})
+		s = bs(res.text, 'lxml')
+		sa = s.select('#app > div.lg-container > article > pre > code')
+		assert len(sa) % 2 == 0
+		ret = []
+		for i in range(0, len(sa), 2):
+			ret.append((sa[i].get_text(), sa[i + 1].get_text()))
+		return ret
+	except Exception as e:
+		print(f'Error {e}')
+		return []
+def save(idx, sam):
+	print(f'Saving {idx}...')
+	try:
+		for i in range(len(sam)):
+			with open(pjoin('problem', f'P{idx}_{i + 1}.in'), 'w') as f:
+				f.write(sam[i][0])
+			with open(pjoin('problem', f'P{idx}_{i + 1}.out'), 'w') as f:
+				f.write(sam[i][1])
+	except Exception as e:
+		print(f'Error {e}')
+l, r = map(int, input('From ?~?: ').split())
+for i in range(l, r + 1):
+	save(i, get(i))
---
+++ web.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+import os, difflib
+from os.path import join as pjoin
+app = Flask('LuoguSampleSearcher')
+f = open('index.html')
+html = f.read()
+f.close()
+def simi(x, y):
+	return int(100 * difflib.SequenceMatcher(None, x, y).quick_ratio())
+@app.get('/')
+def index():
+	return html
+@app.get('/search')
+def search():
+	ret = []
+	ct = request.args.get('content').strip()
+	suf = 'out' if request.args.get('type') == '1' else 'in'
+	for fn in os.listdir('problem'):
+		if fn.split('.')[-1] == suf:
+			with open(pjoin('problem', fn), encoding='utf-8') as f:
+				v = simi(ct, f.read())
+				if v >= 80:
+					ret.append({
+						"PID": int(fn[1:5]),
+						"SID": int(fn[6]),
+						"SIM": v
+					})
+	ret.sort(key=lambda x : -x['SIM'])
+	return jsonify(ret)
+app.run('127.0.0.1', 2333)
